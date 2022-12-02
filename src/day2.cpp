972dbd9ad97e190f86827a7f9376988df3bd3219
Javier Suarez <jsuarez@salesforce.com>
2022-12-02 10:47:24 +0100

    day2 part 2

--- src/day2.cpp
+++ src/day2.cpp
@@ -3,11 +3,25 @@
 #include <unordered_map>
 #include <timer.h>
 
-uint32_t perform(const std::string& input_filename)
+uint32_t perform(const std::string& input_filename, const std::unordered_map<std::string, uint32_t>& map)
 {
     uint32_t total = 0;
 
-    std::unordered_map<std::string, uint32_t> game_map =
+    std::ifstream ifs (input_filename, std::ifstream::in);
+    std::string line;
+
+    while ( getline(ifs, line) )
+    {
+        total += map.at(line);
+    }
+
+    return total;
+}
+
+int main(int /*argc*/, char ** /*argv*/)
+{
+
+    std::unordered_map<std::string, uint32_t> part1_map =
     {
         {"A X", 4},
         {"A Y", 8},
@@ -19,22 +33,26 @@ uint32_t perform(const std::string& input_filename)
         {"C Y", 2},
         {"C Z", 6}
     };
-    std::ifstream ifs (input_filename, std::ifstream::in);
-    std::string line;
 
-    while ( getline(ifs, line) )
+    std::unordered_map<std::string, uint32_t> part2_map =
     {
-        // std::cout << game_map[line] << '\n';
-        total += game_map[line];
-    }
+        {"A X", 3 },
+        {"A Y", 4},
+        {"A Z", 8},
+        {"B X", 1},
+        {"B Y", 5},
+        {"B Z", 9},
+        {"C X", 2},
+        {"C Y", 6},
+        {"C Z", 7}
+    };
 
-    return total;
-}
-
-int main(int /*argc*/, char ** /*argv*/)
-{
     timer t;
-    auto result = perform("../inputs/day2.txt");
+    auto result = perform("../inputs/day2.txt", part1_map);
     std::cout << "day 1 part 1 result: " << result << " in " << t.elapsed_micro() << "us\n";
+
+    t.reset();
+    result = perform("../inputs/day2.txt", part2_map);
+    std::cout << "day 1 part 2 result: " << result << " in " << t.elapsed_micro() << "us\n";
     return 0;
 }
