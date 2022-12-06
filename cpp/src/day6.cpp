0b0e31366e317a99246e4ac182e83a4a9543adab
Javier Suarez <jsuarez@salesforce.com>
2022-12-06 10:59:12 +0100

    day 6 part 2 - cpp

--- cpp/src/day6.cpp
+++ cpp/src/day6.cpp
@@ -3,11 +3,12 @@
 #include <timer.h>
 #include "lib.h"
 
+template<std::size_t N>
 bool is_valid_key(std::string& s)
 {
     std::sort(s.begin(), s.end());
     char c = s[0];
-    for (size_t i=1; i<=3; ++i)
+    for (size_t i=1; i<=(N-1); ++i)
     {
         if ( c == s.at(i) )
         {
@@ -18,17 +19,18 @@ bool is_valid_key(std::string& s)
     return true;
 }
 
+template<std::size_t N>
 size_t perform(const std::string& input_filename)
 {
     auto input = read_input(input_filename).at(0);
     size_t input_length = input.length();
 
-    for (size_t i=0; i<input_length-3; ++i)
+    for (size_t i=0; i<input_length-(N-1); ++i)
     {
-        std::string s = input.substr(i,4);
-        if ( is_valid_key(s) )
+        std::string s = input.substr(i,N);
+        if ( is_valid_key<N>(s) )
         {
-            return i+4;
+            return i+N;
         }
     }
     return 0;
@@ -37,8 +39,11 @@ size_t perform(const std::string& input_filename)
 int main(int /*argc*/, char ** /*argv*/)
 {
     timer t;
-    auto result = perform("../inputs/day6.txt");
+    auto result = perform<4>("../inputs/day6.txt");
     std::cout << "day 6 part 1 result: " << result << " in " << t.elapsed_micro() << "us\n";
 
+    t.reset();
+    result = perform<14>("../inputs/day6.txt");
+    std::cout << "day 6 part 2 result: " << result << " in " << t.elapsed_micro() << "us\n";
     return 0;
 }
