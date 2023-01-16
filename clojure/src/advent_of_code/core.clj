d7ab43c91959931076c80dde2e9ada004d4caa3d
Javier Suarez <jsuarez@salesforce.com>
2023-01-16 15:25:14 +0100

    day 4 - parts 1 & 2 - clojure

--- clojure/src/advent_of_code/core.clj
+++ clojure/src/advent_of_code/core.clj
@@ -1,8 +1,10 @@
 (ns advent-of-code.core
   (:gen-class)
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:require [clojure.set :as set])
+  (:require [clojure.string :as str]))
 
-(defn solve
+(defn day1
   "resolve puzzle"
   [n]
   (->>
@@ -16,8 +18,52 @@
     (take n)
     (reduce +)))
 
+(defn day4
+  "day 4"
+  []
+  (->>
+    ; read the input file
+    (with-open [reader (io/reader "/home/javier/Tech/c++/advent-of-code/advent-of-code-2022/cpp/inputs/day4.txt")]
+      (reduce conj [] (line-seq reader)))
+    ; expand a list with ranges in the pairs
+    (map (fn [j] 
+           (map (fn[i]
+             (range (Integer/parseInt (first i)) 
+               (+ (Integer/parseInt (first (rest i))) 1)))(map #(str/split % #"-")(str/split j #",")))))))
+
+(defn day4-part1
+  "day 4"
+  []
+  (->>
+    (day4)
+    ; is one set contained in the other or viceversa
+    (map #(or
+             (set/subset? (set (first %)) (set (first (rest %))))
+             (set/subset? (set (first (rest %))) (set (first %)))
+           ))
+    (map #(if (true? %) 1 0)) 
+    (reduce +)))
+
+(defn day4-part2
+  "day 4"
+  []
+  (->>
+    (day4)
+    ; calculate the intersecction between the list for each row
+    (map #(some (set (first %)) (first (rest %))))
+    (map #(if (nil? %) 0 1)) 
+    (reduce +)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (solve 1))
-  (println (solve 3)))
+  (cond
+    (= args '("day1"))
+      (do
+        (println (day1 1))
+        (println (day1 3)))
+    (= args '("day4"))
+      (do
+        (println (day4-part1))
+        (println (day4-part2)))
+    :else (println "error: not valid day")))
